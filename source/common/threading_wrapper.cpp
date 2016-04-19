8c81520cc56b057169fb7124746491f34218d49f
Peixuan Zhang <Zhang@PoeticFlower.CN>
2016-04-20 00:25:56 +0800

    COMMON: Add class 'Event' for ThreadingWrapper.

--- source/common/threading_wrapper.cpp
+++ source/common/threading_wrapper.cpp
@@ -38,3 +38,87 @@ void Lock::Release()
   pthread_mutex_unlock(&this->m_handle);
 #endif // _WIN32
 }
+
+Event::Event()
+{
+#ifdef _WIN32
+  this->m_handle = CreateEvent(NULL, FALSE, FALSE, NULL);
+#else  //  POSIX
+  m_counter = 0;
+  if ( pthread_mutex_init(&m_mutex, NULL)
+    || pthread_cond_init (&m_cond,  NULL))
+  {
+    fprintf(stderr,
+      "Event Error: unable to initialize conditional variable\n");
+  }
+#endif // _WIN32
+}
+
+Event::~Event()
+{
+#ifdef _WIN32
+  CloseHandle          (this->m_handle);
+#else  //  POSIX
+  pthread_cond_destroy (&m_cond);
+  pthread_mutex_destroy(&m_mutex);
+#endif // _WIN32
+}
+
+void Event::Wait()
+{
+#ifdef _WIN32
+  WaitForSingleObject (this->m_handle, INFINITE);
+#else  //  POSIX
+  pthread_mutex_lock  (&m_mutex);
+  while (!m_counter)
+    pthread_cond_wait (&m_cond, &m_mutex);
+  m_counter--;
+  pthread_mutex_unlock(&m_mutex);
+#endif // _WIN32
+}
+
+bool Event::TimedWait(uint32_t milliseconds)
+{
+  /* returns true if the wait timed out */
+#ifdef _WIN32
+  return WaitForSingleObject(this->m_handle, milliseconds) == WAIT_TIMEOUT;
+#else  //  POSIX
+  bool timed_out = false;
+  pthread_mutex_lock(&m_mutex);
+  if (!m_counter)
+  {
+    struct timeval  tv;
+    struct timespec ts;
+    gettimeofday(&tv, NULL);
+    // convert current time from (sec, usec) to (sec, nsec)
+    ts.tv_sec   = tv.tv_sec;
+    ts.tv_nsec  = tv.tv_usec * 1000;
+    ts.tv_nsec += 1000 * 1000 * (milliseconds % 1000); // add ms to tv_nsec
+    ts.tv_sec  += ts.tv_nsec / (1000 * 1000 * 1000);   // overflow tv_nsec
+    ts.tv_nsec %= (1000 * 1000 * 1000);                // clamp tv_nsec
+    ts.tv_sec  += milliseconds / 1000;                 // add seconds
+    timed_out   = pthread_cond_timedwait(&m_cond, &m_mutex, &ts) == ETIMEDOUT;
+  }
+  if (m_counter > 0)
+  {
+    m_counter--;
+    timed_out = false;
+  }
+  pthread_mutex_unlock(&m_mutex);
+  return timed_out;
+#endif // _WIN32
+}
+
+void Event::Trigger()
+{
+#ifdef _WIN32
+  SetEvent            (this->m_handle);
+#else  //  POSIX
+  pthread_mutex_lock  (&m_mutex);
+  if (m_counter < UINT32_MAX)
+    m_counter++;
+  /* Signal a single blocking thread */
+  pthread_cond_signal (&m_cond);
+  pthread_mutex_unlock(&m_mutex);
+#endif // _WIN32
+}
--- source/common/threading_wrapper.h
+++ source/common/threading_wrapper.h
@@ -18,3 +18,21 @@ protected:
   pthread_mutex_t  m_handle;
 #endif // _WIN32
 };
+
+class Event
+{
+public:
+  Event();
+  ~Event();
+  void Wait();
+  bool TimedWait(uint32_t milliseconds);
+  void Trigger();
+protected:
+#ifdef _WIN32
+  HANDLE          m_handle;
+#else  //  POSIX
+  pthread_mutex_t m_mutex;
+  pthread_cond_t  m_cond;
+  uint32_t        m_counter;
+#endif // _WIN32
+};
