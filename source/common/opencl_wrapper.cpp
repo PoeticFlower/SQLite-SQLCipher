55b83ba7aad82fd394c591841713c2c6860aaa5e
Peixuan Zhang <Zhang@PoeticFlower.CN>
2016-05-10 00:21:11 +0800

    COMMON: Add function 'AppleGetProcAddress' for OpenCLWrapper.

--- source/common/opencl_wrapper.cpp
+++ source/common/opencl_wrapper.cpp
@@ -53,3 +53,46 @@ unsigned int OpenCLWrapper::m_opencl_func_loading_status = OCL_FUNC_LOAD_INIT;
       action;                                                               \
     }                                                                       \
   } while (0)
+
+#if defined(__APPLE__)
+static void* AppleGetProcAddress(const char* name)
+{
+  static bool    initialized = false;
+  static void   *handle      = NULL;
+  if (!handle && !initialized)
+  {
+    Lock       lock;
+    ScopedLock scoped_lock(lock);
+    if (!initialized)
+    {
+      const char* path =
+        "/System/Library/Frameworks/OpenCL.framework/Versions/Current/OpenCL";
+      handle = dlopen(path, RTLD_LAZY | RTLD_GLOBAL);
+      if (!handle)
+      {
+        OPENCL_LOG_ERROR("Failed to load OpenCL runtime\n");
+      }
+      initialized = true;
+    }
+  }
+  if (!handle)
+    return NULL;
+  return dlsym(handle, name);
+}
+
+#define OPENCL_GET_PROC_ADDRESS(name)                                       \
+  if (NULL == (func_##name = (FUNC_##name)AppleGetProcAddress(#name)))      \
+  {                                                                         \
+    OPENCL_LOG_ERROR("Failed to load OpenCL API: %s\n", #name);             \
+    return false;                                                           \
+  }
+#endif // __APPLE__
+
+#ifndef OPENCL_GET_PROC_ADDRESS
+#ifdef __GNUC__
+#warning("OpenCL dynamic library loader: check configuration")
+#else
+#pragma message("WARNING: OpenCL dynamic library loader: check configuration")
+#endif
+#define OPENCL_GET_PROC_ADDRESS(name) NULL
+#endif // OPENCL_GET_PROC_ADDRESS
