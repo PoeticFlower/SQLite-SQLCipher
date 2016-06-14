4b1d851eed1d1b298074c7039437728c5bc6e461
Peixuan Zhang <Zhang@PoeticFlower.CN>
2016-06-14 13:45:16 +0800

    GUI: Add the implementation of targetver.h.

---
+++ source/gui/targetver.h
@@ -0,0 +1,16 @@
+/*****************************************************************************
+ *
+ * FILE    : targetver.h
+ * PURPOSE : Including SDKDDKVer.h defines the highest available Windows
+ *           platform.
+ * AUTHOR  : Peixuan Zhang <Zhang@PoeticFlower.CN>
+ * COMMENTS: References the example code in Visual Studio.
+ *           If you wish to build your application for a previous Windows
+ *           platform, include WinSDKVer.h and set the _WIN32_WINNT macro to
+ *           the platform you wish to support before including SDKDDKVer.h.
+ *
+ *****************************************************************************/
+
+#pragma once
+
+#include <SDKDDKVer.h>
