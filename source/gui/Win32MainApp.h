3147ec600c34b405cd27c064c1889a4fb08470c9
Peixuan Zhang <Zhang@PoeticFlower.CN>
2016-06-14 13:50:32 +0800

    GUI: Add the implementation of Win32MainApp.h.

---
+++ source/gui/Win32MainApp.h
@@ -0,0 +1,4 @@
+﻿int APIENTRY Win32MainAppEntry(_In_     HINSTANCE hInstance,
+                               _In_opt_ HINSTANCE hPrevInstance,
+                               _In_     LPTSTR    lpCmdLine,
+                               _In_     int       nCmdShow);
