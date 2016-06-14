9c5554342ff487989e49e1cbb25026260488408d
Peixuan Zhang <Zhang@PoeticFlower.CN>
2016-06-14 13:45:25 +0800

    GUI: Add the implementation of stdafx.cpp.

---
+++ source/gui/stdafx.cpp
@@ -0,0 +1,15 @@
+/*****************************************************************************
+ *
+ * FILE    : stdafx.cpp
+ * PURPOSE : Standard Application Framework Extensions.
+ * AUTHOR  : Peixuan Zhang <Zhang@PoeticFlower.CN>
+ * COMMENTS: References the example code in Visual Studio.
+ *           Source file that includes just the standard includes.
+ *           gui_main.pch will be the pre-compiled header.
+ *           stdafx.obj will contain the pre-compiled type information.
+ *           Reference any additional headers you need in STDAFX.H and not in
+ *           this file.
+ *
+ *****************************************************************************/
+
+#include "stdafx.h"
